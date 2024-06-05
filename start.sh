ff553629140d7cddbd046e817cbd4ac9af9aecca
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2024-06-05 10:53:00 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,8 +11,8 @@ function log() {
    echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ") [controller] [${level}] ${msg}"
 }
 
-if ((TOR_INSTANCES < 1 || TOR_INSTANCES > 40)); then
-   log "fatal" "Environment variable TOR_INSTANCES has to be within the range of 1...40"
+if ((TOR_INSTANCES < 1 || TOR_INSTANCES > 1000)); then
+   log "fatal" "Environment variable TOR_INSTANCES has to be within the range of 1...1000"
    exit 1
 fi
 
